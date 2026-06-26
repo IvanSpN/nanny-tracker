9aec256da648191aaad7f9d2368a895c88234053
Ivan S <ivanspn67@gmail.com>
2026-06-26 12:10:10 +0500

    feat(front): test husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'frontend/**/*.{js,jsx,ts,tsx}': [
     'npm --prefix frontend exec prettier -- --write',
-    'npm --prefix frontend exec eslint -- --fix',
+    'npm --prefix frontend exec eslint -- --fix --config frontend/eslint.config.mjs',
   ],
   'frontend/**/*.{json,css,md,mjs}': ['npm --prefix frontend exec prettier -- --write'],
 };
