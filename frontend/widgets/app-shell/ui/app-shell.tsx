453160b12f95f904c212b5d4b340c6f3e0a31e97
Ivan S <ivanspn67@gmail.com>
2026-08-05 10:12:53 +0500

    test CICD

--- frontend/widgets/app-shell/ui/app-shell.tsx
+++ frontend/widgets/app-shell/ui/app-shell.tsx
@@ -96,7 +96,7 @@ export function AppShell() {
               </div>
               <div>
                 <p className="text-sm font-semibold">Nanny Tracker</p>
-                <p className="text-xs text-muted-foreground">Кабинет работника</p>
+                <p className="text-xs text-muted-foreground">Кабинет работничка</p>
               </div>
             </div>
 
